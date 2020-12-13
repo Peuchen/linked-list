b973d460dab9e8965a7b90fa17e2940dbc31d739
peuchen <npeuchen@gmail.com>
2020-12-13 17:41:04 +0100

    Create prepend, append, size, head and tail methods

--- linked-list.rb
+++ linked-list.rb
@@ -1,21 +1,72 @@
 class Node
-  attr_accessor :value
-  def value
-    @value = nil
-  end
+  attr_accessor :value, :next_node
 
-  def next_node
+  @@count = 0
+
+  def initialize
+    @@count += 1
+
+    @value = nil
     @next_node = nil
   end
+
+  def self.count
+    @@count
+  end
+
 end
 
 class LinkedList
-  def append(value)
-    node = Node.new
-    node.value = value
+
+  def initialize
+    @head = nil
+    @tail = nil
   end
+
+  def prepend(value)
+    if @head.nil?
+      @head = Node.new
+      @tail = @head
+      @head.value = value
+      @tail.value = value
+    else
+      @head.next_node = head
+      @head = Node.new
+      @head.value = value
+    end
+  end
+
+  def append(value)
+    if @head.nil?
+      @tail = Node.new
+      @head.value = value
+      @tail.value = value
+    else
+      @tail.next_node = tail
+      @tail = Node.new
+      @tail.value = value
+    end
+  end
+
+  def size
+    Node.count
+  end
+
+  def head
+    @head.value
+  end
+
+  def tail
+    @tail.value
+  end
+
 end
 
 list = LinkedList.new
 puts list
-list.append("test")
+list.prepend("test2")
+list.prepend("test1")
+list.prepend("test0")
+puts list.size
+puts list.head
+puts list.tail
