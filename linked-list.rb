007c694697dbd4f85fec7c72344d13cd64e4167d
peuchen <npeuchen@gmail.com>
2020-12-13 16:22:13 +0100

    Create append method

--- linked-list.rb
+++ linked-list.rb
@@ -1,16 +1,21 @@
 class Node
+  attr_accessor :value
   def value
-    nil
+    @value = nil
   end
 
   def next_node
-    nil
+    @next_node = nil
   end
 end
 
 class LinkedList
   def append(value)
     node = Node.new
-    node.value(value)
+    node.value = value
   end
 end
+
+list = LinkedList.new
+puts list
+list.append("test")
